71acc09eef0767ad6afee3fd9efab1c4bfe33ff6
Reza Rezaeian <70855136+Iceage7@users.noreply.github.com>
2023-09-03 21:04:14 +0330

    Update main.py with wandb for parameter sweep (hidden_layers)

--- Skyformer/src/main.py
+++ Skyformer/src/main.py
@@ -6,12 +6,14 @@ import math
 import json
 import time
 import itertools
+import wandb
 from tqdm import tqdm
 import numpy as np
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
+import datetime
 
 from utils import redirect_stdout
 from config import Config
@@ -121,6 +123,14 @@ def step_LRA(model, optimizer, lr_scheduler, ds_iter,amp_scaler,
     summary[component]["loss"].append(loss)
     summary[component]["accu"].append(accu)
 
+    if component == 'train':
+
+        metrics = {"train/train_loss": loss,
+                   "train/learning_rate" : learning_rate,
+                   "train/accuracy" : accu, 
+                "train/step": step_idx}
+        wandb.log(metrics)
+
     if writer is not None:
         writer.add_scalar('loss', loss, step_idx)
         writer.add_scalar('accu', accu, step_idx)
@@ -153,6 +163,12 @@ def train_LRA(model, optimizer, lr_scheduler, ds_iter, amp_scaler,
             # if  dev_loss < best_dev_loss:
             #     best_dev_loss = dev_loss
             dev_accu = np.mean(summary["dev"]["accu"])
+
+            val_metrics = {"val/val_loss": np.mean(summary["dev"]["loss"]), 
+                "val/val_accuracy": np.mean(summary["dev"]["accu"])}
+            
+            wandb.log = {val_metrics}
+
             if dev_accu > best_dev_accu:
                 best_dev_accu = dev_accu
                 if (train_step_idx + 1) > total_step * 0.2:
@@ -169,6 +185,10 @@ def train_LRA(model, optimizer, lr_scheduler, ds_iter, amp_scaler,
     print('total training step (k): {}'.format(total_step/1000.0))
     print("total training time (s): {}".format(int(time.time()-init_t)))
     print("peak memory usage (MB): {}".format(torch.cuda.memory_stats()['active_bytes.all.peak']>>20))
+
+    wandb.summary["training_steps"] = total_step
+    wandb.summary["training_time"] = total_step(int(time.time()-init_t))
+    wandb.summary["memory_usage"] = torch.cuda.memory_stats()['active_bytes.all.peak']>>20
 
 
 def eval_LRA(model, optimizer, lr_scheduler, ds_iter, amp_scaler,
@@ -195,133 +215,163 @@ def get_args():
     parser.add_argument("--task", type = str, default="lra-listops",
                         help = "lra-listops, lra-retrieval, lra-text, lra-pathfinder32-curv_contour_length_14")
     parser.add_argument('--random', type=int, default=42)
+    parser.add_argument('--full_training', type=bool, default=True)
+    parser.add_argument('--sweep_id', type=str)
     args = parser.parse_args()
     return args
 
+
+def run_sweep(config=None):
+
+    # Initialize a new wandb run
+    with wandb.init(config=config):
+        # If called by wandb.agent, as below,
+        # this config will be set by Sweep Controller
+
+        args = get_args()
+
+        sweep_config = wandb.config
+
+        wandb.summary["full_training"] =  args.full_training
+
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+        if args.task == 'lra-pathfinder':
+            args.task = 'lra-pathfinder32-curv_contour_length_14'
+
+        
+        
+        ### get model config ###
+        model_config = Config[args.task]["model"]
+        if args.attn in Config[args.task]["extra_attn_config"]:
+            model_config.update(Config[args.task]["extra_attn_config"][args.attn])
+        model_config["mixed_precision"] = True
+        model_config["attn_type"] = args.attn
+        model_config["max_seq_len"] = int(2 ** math.ceil(math.log2(model_config["max_seq_len"])))
+        model_config["random_seed"] = args.random
+
+        model_config["hidden_size"] = sweep_config.hidden_size
+
+        training_config = Config[args.task]["training"]
+
+        if not args.full_training:
+            training_config['num_train_steps'] = 2000   
+            training_config["eval_frequency"] = 100
+
+        ### log preparation ###
+        # log_dir = './log-{}/'.format(args.random)
+        log_dir = './log-{}/{}'.format(args.random, timestamp)
+        if not os.path.exists(log_dir):
+            os.mkdir(log_dir)
+        log_dir = os.path.join(log_dir, args.task)
+        if not os.path.exists(log_dir):
+            os.mkdir(log_dir)
+
+        log_path = os.path.join(log_dir,'{}.{}.log'.format(args.mode, args.checkpoint))
+        redirect_stdout(open(log_path, 'w'))
+        summary = {
+            component:{"t":0, "loss":[], "accu":[], "best_accu":0, "component":component}
+            for component in ["train", "dev", "test"]
+        }
+        writer = SummaryWriter(os.path.join(log_dir,'{}.tensorboard'.format(args.checkpoint)))
+
+        print(json.dumps([model_config, training_config], indent = 4))
+
+
+        ###  set the random seeds for deterministic results. ####
+        SEED = args.random
+        random.seed(SEED)
+        torch.manual_seed(SEED)
+        torch.backends.cudnn.deterministic = True
+
+
+
+        ### model preparation ###
+        if args.task == "lra-retrieval":
+            model = ModelForSCDual(model_config)
+        else:
+            model = ModelForSC(model_config)
+
+
+        # checkpoint_dir = './checkpoints-{}'.format(args.random)
+        checkpoint_dir = './checkpoints-{}/{}'.format(args.random, timestamp)
+        if not os.path.exists(checkpoint_dir):
+            os.mkdir(checkpoint_dir)
+        checkpoint_path = os.path.join(checkpoint_dir, '{}.{}.model'.format(args.checkpoint, args.random))
+        training_config["checkpoint_path"] = checkpoint_path
+        if os.path.exists(checkpoint_path):
+            checkpoint = torch.load(checkpoint_path)
+            model.load_state_dict(checkpoint["model_state_dict"])
+            print("model loaded from: " + checkpoint_path)
+
+
+        model = model.cuda()
+        print(model)
+        num_parameters = np.sum([np.prod(weight.size()) for weight in model.parameters()])
+        print(f"parameter_size: {[weight.size() for weight in model.parameters()]}", flush = True)
+        print(f"num_parameter: {num_parameters}", flush = True)
+ 
+        wandb.summary["num_parameter"] = num_parameters
+
+        device_ids = list(range(torch.cuda.device_count()))
+        # print(f"GPU list: {device_ids}")
+        # model = nn.DataParallel(model, device_ids = device_ids)
+
+
+
+        ### data preparation ###
+
+        ds_iter = {
+            "train":enumerate(DataLoader(LRADataset(f"./data/lra_processed/{args.task}.train.pickle", True), batch_size = training_config["batch_size"], drop_last = True)),
+            "dev":enumerate(DataLoader(LRADataset(f"./data/lra_processed/{args.task}.dev.pickle", True), batch_size = training_config["batch_size"], drop_last = True)),
+            "test":enumerate(DataLoader(LRADataset(f"./data/lra_processed/{args.task}.test.pickle", False), batch_size = training_config["batch_size"], drop_last = True)),
+        }
+
+        ### training preparation ###
+
+        optimizer = torch.optim.AdamW(
+            model.parameters(),
+            lr = training_config["learning_rate"],
+            betas = (0.9, 0.999), eps = 1e-6, weight_decay = training_config["weight_decay"]
+        )
+
+        lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            optimizer = optimizer,
+            max_lr = training_config["learning_rate"],
+            pct_start = training_config["warmup"] / training_config["num_train_steps"],
+            anneal_strategy = training_config["lr_decay"],
+            total_steps = training_config["num_train_steps"]
+        )
+
+        amp_scaler = torch.cuda.amp.GradScaler() if model_config["mixed_precision"] else None
+
+
+        # accumu_steps = max(training_config["batch_size"] // len(device_ids) // model_config["gpu_memory"], 1)
+        accumu_steps = model_config["bz_rate"] if "bz_rate" in model_config else 1
+        # accumu_steps = 1
+        print(f"accumu_steps={accumu_steps}")
+        training_config['accumu_steps'] = accumu_steps
+
+
+
+        ### train ###
+        if args.mode == 'train':
+            train_LRA(model, optimizer, lr_scheduler, ds_iter, amp_scaler,
+                    training_config, summary, writer)
+
+        ### eval ###
+        if os.path.exists(checkpoint_path) and checkpoint_path != './checkpoints/test.model':
+            checkpoint = torch.load(checkpoint_path)
+            model.load_state_dict(checkpoint["model_state_dict"])
+            print("loading the best model from: " + checkpoint_path)
+        eval_LRA(model, optimizer, lr_scheduler, ds_iter, amp_scaler,
+                training_config, summary)
+
+
+
 def main():
     args = get_args()
-
-    if args.task == 'lra-pathfinder':
-        args.task = 'lra-pathfinder32-curv_contour_length_14'
-
-
-    ### get model config ###
-    model_config = Config[args.task]["model"]
-    if args.attn in Config[args.task]["extra_attn_config"]:
-        model_config.update(Config[args.task]["extra_attn_config"][args.attn])
-    model_config["mixed_precision"] = True
-    model_config["attn_type"] = args.attn
-    model_config["max_seq_len"] = int(2 ** math.ceil(math.log2(model_config["max_seq_len"])))
-    model_config["random_seed"] = args.random
-
-    training_config = Config[args.task]["training"]
-
-    ### log preparation ###
-    log_dir = './log-{}/'.format(args.random)
-    if not os.path.exists(log_dir):
-        os.mkdir(log_dir)
-    log_dir = os.path.join(log_dir, args.task)
-    if not os.path.exists(log_dir):
-        os.mkdir(log_dir)
-
-    log_path = os.path.join(log_dir,'{}.{}.log'.format(args.mode, args.checkpoint))
-    redirect_stdout(open(log_path, 'w'))
-    summary = {
-        component:{"t":0, "loss":[], "accu":[], "best_accu":0, "component":component}
-        for component in ["train", "dev", "test"]
-    }
-    writer = SummaryWriter(os.path.join(log_dir,'{}.tensorboard'.format(args.checkpoint)))
-
-    print(json.dumps([model_config, training_config], indent = 4))
-
-
-    ###  set the random seeds for deterministic results. ####
-    SEED = args.random
-    random.seed(SEED)
-    torch.manual_seed(SEED)
-    torch.backends.cudnn.deterministic = True
-
-
-
-    ### model preparation ###
-    if args.task == "lra-retrieval":
-        model = ModelForSCDual(model_config)
-    else:
-        model = ModelForSC(model_config)
-
-
-    checkpoint_dir = './checkpoints-{}'.format(args.random)
-    if not os.path.exists(checkpoint_dir):
-        os.mkdir(checkpoint_dir)
-    checkpoint_path = os.path.join(checkpoint_dir, '{}.{}.model'.format(args.checkpoint, args.random))
-    training_config["checkpoint_path"] = checkpoint_path
-    if os.path.exists(checkpoint_path):
-        checkpoint = torch.load(checkpoint_path)
-        model.load_state_dict(checkpoint["model_state_dict"])
-        print("model loaded from: " + checkpoint_path)
-
-
-    model = model.cuda()
-    print(model)
-    print(f"parameter_size: {[weight.size() for weight in model.parameters()]}", flush = True)
-    print(f"num_parameter: {np.sum([np.prod(weight.size()) for weight in model.parameters()])}", flush = True)
-
-
-    device_ids = list(range(torch.cuda.device_count()))
-    # print(f"GPU list: {device_ids}")
-    # model = nn.DataParallel(model, device_ids = device_ids)
-
-
-
-    ### data preparation ###
-
-    ds_iter = {
-        "train":enumerate(DataLoader(LRADataset(f"./data/lra_processed/{args.task}.train.pickle", True), batch_size = training_config["batch_size"], drop_last = True)),
-        "dev":enumerate(DataLoader(LRADataset(f"./data/lra_processed/{args.task}.dev.pickle", True), batch_size = training_config["batch_size"], drop_last = True)),
-        "test":enumerate(DataLoader(LRADataset(f"./data/lra_processed/{args.task}.test.pickle", False), batch_size = training_config["batch_size"], drop_last = True)),
-    }
-
-    ### training preparation ###
-
-    optimizer = torch.optim.AdamW(
-        model.parameters(),
-        lr = training_config["learning_rate"],
-        betas = (0.9, 0.999), eps = 1e-6, weight_decay = training_config["weight_decay"]
-    )
-
-    lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(
-        optimizer = optimizer,
-        max_lr = training_config["learning_rate"],
-        pct_start = training_config["warmup"] / training_config["num_train_steps"],
-        anneal_strategy = training_config["lr_decay"],
-        total_steps = training_config["num_train_steps"]
-    )
-
-    amp_scaler = torch.cuda.amp.GradScaler() if model_config["mixed_precision"] else None
-
-
-    # accumu_steps = max(training_config["batch_size"] // len(device_ids) // model_config["gpu_memory"], 1)
-    accumu_steps = model_config["bz_rate"] if "bz_rate" in model_config else 1
-    # accumu_steps = 1
-    print(f"accumu_steps={accumu_steps}")
-    training_config['accumu_steps'] = accumu_steps
-
-
-
-    ### train ###
-    if args.mode == 'train':
-        train_LRA(model, optimizer, lr_scheduler, ds_iter, amp_scaler,
-                  training_config, summary, writer)
-
-    ### eval ###
-    if os.path.exists(checkpoint_path) and checkpoint_path != './checkpoints/test.model':
-        checkpoint = torch.load(checkpoint_path)
-        model.load_state_dict(checkpoint["model_state_dict"])
-        print("loading the best model from: " + checkpoint_path)
-    eval_LRA(model, optimizer, lr_scheduler, ds_iter, amp_scaler,
-             training_config, summary)
-
-
+    wandb.agent(args.sweep_id, run_sweep)
 
 if __name__ == '__main__':
     main()
