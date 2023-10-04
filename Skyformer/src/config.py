7bfcd256ed614fb6a33d4333d64693c7fb0dec34
Reza Rezaeian <70855136+Iceage7@users.noreply.github.com>
2023-10-04 10:49:18 +0330

    Update config.py

--- Skyformer/src/config.py
+++ Skyformer/src/config.py
@@ -43,6 +43,7 @@ listops = {
                   "softmaxRBF32":{"bz_rate":1},
                   "kernelized":{"bz_rate":1},
                   "mlp":{"bz_rate":1,},
+                  "mlpconcat":{"bz_rate":1,},
                   "conv":{"bz_rate":1,},
                   "dct":{"bz_rate":1},
                   "sketchedRBF32128":{"bz_rate":1, "nb_features":128, "sketched_kernel":"kernel_RS_RBF", "accumulation":1, "sampling_factor":4, "no_projection":False},
